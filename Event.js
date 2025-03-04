c03e85a414be639eb592ada41728cde1ff2ef3aa
Omar <omarsherifmohammedomar@gmail.com>
2025-03-04 20:35:54 +0200

    event

---
+++ Event.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/ETS")
+
+const eventSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    date: {
+        type: Date,
+        required: true,
+    },
+    location: {
+        type: String,
+        required: true,
+    },
+    category: {
+        type: String,
+        required: true,
+    },
+    image: {
+        type: Image,
+        required: true,
+    }
+})
