207d382f1a539f0da2e2e93776f3d0243550525c
mourad2213 <ahmed.mourad338@gmail.com>
2025-04-10 00:50:35 +0200

    nnnnn

---
+++ routes/exampleRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const { getExamples, createExample } = require('../controllers/exampleController');
+
+router.route('/')
+  .get(getExamples)
+  .post(createExample);
+
+module.exports = router;
