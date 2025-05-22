a234e366bfa2a004c2643ed79b6c512a13a95550
@ <@>
2025-05-22 21:23:55 +0300

    Event list animation and some bug fixs and some other small stuff

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,7 +1,21 @@
 import "./styles/NavBar.css"
 import { Link, useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
+
 function NavBar() {
   const location = useLocation();
+  const [isLoggedIn, setIsLoggedIn] = useState(!!localStorage.getItem("user"));
+
+  useEffect(() => {
+    function handleStorage() {
+      setIsLoggedIn(!!localStorage.getItem("user"));
+    }
+    window.addEventListener("storage", handleStorage);
+    // Update state on mount in case login/logout happens in this tab
+    handleStorage();
+    return () => window.removeEventListener("storage", handleStorage);
+  }, []);
+
   return (
     <div className="navmain">
       <nav className="navbar">
@@ -12,15 +26,24 @@ function NavBar() {
           <li>
             <Link to="/" className={location.pathname === "/" ? "active" : ""}>Home</Link>
           </li>
-          <li>
-            <Link to="/login" className={location.pathname === "/login" ? "active" : ""}>Login</Link>
-          </li>
-          <li>
-            <Link to="/register" className={location.pathname === "/register" ? "active" : ""}>Register</Link>
-          </li>
+          {!isLoggedIn && (
+            <>
+              <li>
+                <Link to="/login" className={location.pathname === "/login" ? "active" : ""}>Login</Link>
+              </li>
+              <li>
+                <Link to="/register" className={location.pathname === "/register" ? "active" : ""}>Register</Link>
+              </li>
+            </>
+          )}
+          {isLoggedIn && (
+            <li>
+              <Link to="/profile" className={location.pathname === "/profile" ? "active" : ""}>My Profile</Link>
+            </li>
+          )}
         </ul>
       </nav>
     </div>
   );
 }
-export default NavBar;  
+export default NavBar;
